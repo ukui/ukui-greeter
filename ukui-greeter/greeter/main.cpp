d124a5ae74e2951183f153bb5ff758b78996a5c0
liuyuanpeng <liuyuanpeng@kylinos.cn>
2020-04-07 10:53:54 +0800

    add Qt::AA_EnableHighDpiScaling Qt::AA_UseHighDpiPixmaps

--- ukui-greeter/greeter/main.cpp
+++ ukui-greeter/greeter/main.cpp
@@ -89,6 +89,12 @@ void waitMonitorsReady()
 int main(int argc, char *argv[])
 {
     qInstallMessageHandler(outputMessage);
+    
+#if(QT_VERSION>=QT_VERSION_CHECK(5,6,0))
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+    
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(true);
 
