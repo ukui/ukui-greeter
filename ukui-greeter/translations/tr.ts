0bafcb623b8528afe4d238f01988c0fddf75d6e0
Aylinux <cihanalk@gmail.com>
2020-07-04 00:28:24 +0300

    Update tr.ts

--- ukui-greeter/translations/tr.ts
+++ ukui-greeter/translations/tr.ts
@@ -5,35 +5,41 @@
     <name>BioAuthentication</name>
     <message>
         <source>authentication failed, restart after 2 seconds</source>
-        <translation type="obsolete">kimlik doğrulama başarısız, 2 saniye sonra yeniden başlatılacak</translation>
+        <translation type="obsolete">Kimlik doğrulama başarısız, 2 saniye sonra yeniden başlat</translation>
     </message>
 </context>
 <context>
     <name>BioDeviceView</name>
     <message>
+        <source>password login&lt;&lt;</source>
+        <translation type="obsolete">Şifrenizi girin&lt;&lt;</translation>
+    </message>
+    <message>
         <source>password login</source>
-        <translation type="obsolete">giriş parolası</translation>
+        <translation type="obsolete">Şifrenizi girin</translation>
     </message>
     <message>
         <source>fingerprint</source>
-        <translation type="obsolete">parmak izi</translation>
+        <translation type="obsolete">Parmak izi</translation>
     </message>
     <message>
         <source>fingerevin</source>
-        <translation type="obsolete">damar izi</translation>
+        <translation type="obsolete">Damar izi</translation>
     </message>
     <message>
         <source>iris</source>
-        <translation type="obsolete">göz</translation>
+        <translation type="obsolete">Göz izi</translation>
     </message>
 </context>
 <context>
     <name>BiometricAuthWidget</name>
     <message>
+        <location filename="../BiometricAuth/biometricauthwidget.cpp" line="100"/>
         <source>Current device: </source>
-        <translation>Şuanki aygıt: </translation>
+        <translation>Mevcut aygıt: </translation>
     </message>
     <message>
+        <location filename="../BiometricAuth/biometricauthwidget.cpp" line="163"/>
         <source>Identify failed, Please retry.</source>
         <translation>Tanımlama başarısız, Lütfen tekrar deneyin.</translation>
     </message>
@@ -41,18 +47,22 @@
 <context>
     <name>BiometricDevicesWidget</name>
     <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="50"/>
         <source>Please select the biometric device</source>
         <translation>Lütfen biyometrik cihazı seçin</translation>
     </message>
     <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="55"/>
         <source>Device type:</source>
         <translation>Atgıt türü:</translation>
     </message>
     <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="72"/>
         <source>Device name:</source>
         <translation>Aygıt Adı:</translation>
     </message>
     <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="82"/>
         <source>OK</source>
         <translation>Tamam</translation>
     </message>
@@ -60,22 +70,26 @@
 <context>
     <name>DeviceType</name>
     <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="37"/>
         <source>FingerPrint</source>
         <translation>Parmak izi</translation>
     </message>
     <message>
         <source>FingerVein</source>
-        <translation>parmak damarı</translation>
+        <translation>Damar izi</translation>
     </message>
     <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="41"/>
         <source>Iris</source>
-        <translation>Göz</translation>
+        <translation>Göz izi</translation>
     </message>
     <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="43"/>
         <source>Face</source>
-        <translation>Yüz</translation>
+        <translation>Yüz tanıma</translation>
     </message>
     <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="45"/>
         <source>VoicePrint</source>
         <translation>Ses izi</translation>
     </message>
@@ -83,100 +97,161 @@
 <context>
     <name>FakeDialog</name>
     <message>
+        <location filename="../common/fakedialog.cpp" line="63"/>
         <source>cancel</source>
-        <translation>iptal</translation>
+        <translation>İptal</translation>
     </message>
 </context>
 <context>
     <name>GreeterWindow</name>
     <message>
-        <source>CN</source>
-        <translation type="obsolete">CN</translation>
+        <source>Zh</source>
+        <translation type="obsolete">Zh</translation>
     </message>
     <message>
-        <source>EN</source>
-        <translation type="obsolete">TR</translation>
+        <source>En</source>
+        <translation type="obsolete">En</translation>
+    </message>
+    <message>
+        <source>Guest</source>
+        <translation>Misafir</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation type="obsolete">İngilizce</translation>
+    </message>
+    <message>
+        <source>Chinese</source>
+        <translation type="obsolete">Çince</translation>
     </message>
 </context>
 <context>
     <name>GreeterWrapper</name>
     <message>
+        <location filename="../greeter/greeterwrapper.cpp" line="105"/>
         <source>failed to start session.</source>
-        <translation>oturum başlatılamadı.</translation>
+        <translation>Oturum başlatılamadı.</translation>
     </message>
 </context>
 <context>
     <name>IconEdit</name>
     <message>
         <source>Login</source>
-        <translation type="obsolete">Giriş</translation>
+        <translation>Giriş</translation>
     </message>
 </context>
 <context>
     <name>KeyboardWidget</name>
     <message>
+        <location filename="../VirtualKeyboard/src/keyboardwidget.ui" line="29"/>
         <source>KeyboardWidget</source>
-        <translation>Klavye Widget&apos;ı</translation>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LanguageWidget</name>
+    <message>
+        <source>Please select the language of session</source>
+        <translation type="vanished">Lütfen oturum dilini seçin</translation>
     </message>
 </context>
 <context>
     <name>LoginWindow</name>
     <message>
-        <source>Login</source>
+        <location filename="../greeter/loginwindow.cpp" line="312"/>
+        <source>logged in</source>
         <translation>Giriş</translation>
     </message>
     <message>
-        <source>logged in</source>
-        <translation>giriş</translation>
-    </message>
-    <message>
+        <location filename="../greeter/loginwindow.cpp" line="392"/>
         <source>login</source>
-        <translation>giriş</translation>
+        <translation>Giriş</translation>
     </message>
     <message>
+        <location filename="../greeter/loginwindow.cpp" line="539"/>
+        <source>Incorrect user name, please input again</source>
+        <translation>Yanlış kullanıcı adı, lütfen tekrar girin</translation>
+    </message>
+    <message>
+        <location filename="../greeter/loginwindow.cpp" line="754"/>
+        <source>Biometric Authentication</source>
+        <translation>Biyometrik Kimlik Doğrulama</translation>
+    </message>
+    <message>
+        <location filename="../greeter/loginwindow.cpp" line="767"/>
+        <source>Password Authentication</source>
+        <translation>Şifre Doğrulama</translation>
+    </message>
+    <message>
+        <location filename="../greeter/loginwindow.cpp" line="779"/>
+        <source>Other Devices</source>
+        <translation>Diğer Aygıtlar</translation>
+    </message>
+    <message>
+        <location filename="../greeter/loginwindow.cpp" line="580"/>
+        <location filename="../greeter/loginwindow.cpp" line="789"/>
+        <source>Retry</source>
+        <translation>Yeniden Dene</translation>
+    </message>
+    <message>
+        <source>please select the type of authentication</source>
+        <translation type="obsolete">Kütfen kimlik doğrulama türünü seçin</translation>
+    </message>
+    <message>
+        <source>Please select the type of authentication</source>
+        <translation type="obsolete">Lütfen kimlik doğrulama türünü seçin</translation>
+    </message>
+    <message>
+        <source>Please select the device of authentication</source>
+        <translation type="obsolete">Lütfen kimlik doğrulama cihazını seçin</translation>
+    </message>
+    <message>
+        <source>Guest</source>
+        <translation type="obsolete">Misafir</translation>
+    </message>
+    <message>
+        <location filename="../greeter/loginwindow.cpp" line="480"/>
         <source>Password: </source>
         <translation>Parola: </translation>
     </message>
     <message>
         <source>login:</source>
-        <translation type="vanished">giriş:</translation>
+        <translation type="vanished">Giriş:</translation>
     </message>
     <message>
+        <source>Password:</source>
+        <translation type="obsolete">Parola:</translation>
+    </message>
+    <message>
+        <location filename="../greeter/loginwindow.cpp" line="544"/>
+        <location filename="../greeter/loginwindow.cpp" line="549"/>
         <source>Incorrect password, please input again</source>
-        <translation>parola hatalı, lütfen tekrar girin</translation>
+        <translation>Parola yanlış, lütfen tekrar deneyin</translation>
     </message>
     <message>
-        <source>please select the type of authentication</source>
-        <translation type="obsolete">lütfen kimlik doğrulama türünü seçin</translation>
+        <location filename="../greeter/loginwindow.cpp" line="267"/>
+        <location filename="../greeter/loginwindow.cpp" line="575"/>
+        <source>Login</source>
+        <translation>Giriş</translation>
     </message>
     <message>
-        <source>Incorrect user name, please input again</source>
-        <translation>Kullanıcı adı hatalı lütfen tekrar girin</translation>
-    </message>
-    <message>
-        <source>Retry</source>
-        <translation>Yeniden dene</translation>
-    </message>
-    <message>
-        <source>Biometric Authentication</source>
-        <translation>Biyometrik Kimlik Doğrulama</translation>
-    </message>
-    <message>
-        <source>Password Authentication</source>
-        <translation>Şifre Doğrulama</translation>
-    </message>
-    <message>
-        <source>Other Devices</source>
-        <translation>Diğer Aygıtlar</translation>
-    </message>
-    <message>
+        <location filename="../greeter/loginwindow.cpp" line="482"/>
         <source>Username</source>
-        <translation>Kullanıcı adı</translation>
+        <translation>Kullanıcı Adı</translation>
+    </message>
+    <message>
+        <source>Failed to start session</source>
+        <translation type="obsolete">Oturum başlatılamadı</translation>
+    </message>
+    <message>
+        <source>password error, please input again</source>
+        <translation type="obsolete">Hatalı şifre girdiniz, lütfen tekrar deneyin</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../display-switch/ui_mainwindow.h" line="127"/>
         <source>MainWindow</source>
         <translation>Ana Ekran</translation>
     </message>
@@ -184,66 +259,89 @@
 <context>
     <name>PowerManager</name>
     <message>
-        <source>suspend</source>
-        <translation type="obsolete">askıya al</translation>
-    </message>
-    <message>
-        <source>restart</source>
-        <translation type="obsolete">yeniden başlat</translation>
-    </message>
-    <message>
-        <source>shutdown</source>
-        <translation type="obsolete">kapat</translation>
-    </message>
-    <message>
-        <source>Suspend</source>
-        <translation>Askıya Al</translation>
-    </message>
-    <message>
-        <source>Restart</source>
-        <translation>Yeniden Başlat</translation>
+        <source>SwitchUser</source>
+        <translation type="vanished">Kullanıcı Değiştir</translation>
     </message>
     <message>
         <source>Switch User</source>
         <translation>Kullanıcı Değiştir</translation>
     </message>
     <message>
+        <source>Suspend</source>
+        <translation>Askıya Al</translation>
+    </message>
+    <message>
+        <location filename="../greeter/powerwindow.cpp" line="144"/>
+        <source>Sleep</source>
+        <translation>Uyku Modu</translation>
+    </message>
+    <message>
+        <location filename="../greeter/powerwindow.cpp" line="158"/>
+        <source>Restart</source>
+        <translation>Yeniden Başlat</translation>
+    </message>
+    <message>
+        <location filename="../greeter/powerwindow.cpp" line="172"/>
         <source>Power Off</source>
-        <translation>Kapat</translation>
+        <translation>Bilgisayarı Kapat</translation>
+    </message>
+    <message>
+        <source>Shutdown</source>
+        <translation type="vanished">Bilgisayarı Kapat</translation>
+    </message>
+    <message>
+        <source>suspend</source>
+        <translation type="vanished">Beklemeye Al</translation>
+    </message>
+    <message>
+        <source>restart</source>
+        <translation type="vanished">Yeniden Başlat</translation>
+    </message>
+    <message>
+        <source>shutdown</source>
+        <translation type="vanished">Bilgisayarı Kapat</translation>
     </message>
 </context>
 <context>
     <name>PowerWindow</name>
     <message>
         <source>Goodbye. Would you like to…</source>
-        <translation type="obsolete">Güle güle. ister misiniz…</translation>
+        <translation type="obsolete">Güle güle. Tekrar bekleriz...</translation>
+    </message>
+    <message>
+        <source>Goodbye. Would you like to...</source>
+        <translation type="vanished">Güle güle. Tekrar bekleriz...</translation>
     </message>
     <message>
         <source>Other users are currently logged in to this computer, shutting down now will also close these other sessions.</source>
-        <translation type="vanished">Şu anda bu bilgisayarda oturum açmış olan diğer kullanıcılar var, şimdi kapatıldığında bu diğer oturumlar da kapatılacaktır.</translation>
+        <translation type="vanished">Şu anda bu bilgisayarda oturum açmış diğer kullanıcılar var, şimdi kapatıldığında bu diğer oturumlar da kapatılacaktır.</translation>
     </message>
     <message>
         <source>suspend</source>
-        <translation type="vanished">askıya al</translation>
+        <translation type="vanished">Beklemeye Al</translation>
     </message>
     <message>
         <source>hibernate</source>
-        <translation type="vanished">hazırda beklet</translation>
+        <translation type="vanished">Derin Uyku</translation>
     </message>
     <message>
         <source>restart</source>
-        <translation type="vanished">yeniden başlat</translation>
+        <translation type="vanished">Yeniden Başlat</translation>
     </message>
     <message>
         <source>shutdown</source>
-        <translation type="vanished">kapat</translation>
+        <translation type="vanished">Bilgisayarı Kapat</translation>
     </message>
 </context>
 <context>
     <name>SessionWindow</name>
     <message>
         <source>select the desktop environment</source>
-        <translation type="obsolete">masaüstü ortamını seçin</translation>
+        <translation type="obsolete">Masaüstü ortamını seçin</translation>
+    </message>
+    <message>
+        <source>Please select the desktop environment</source>
+        <translation type="vanished">Lütfen masaüstü ortamını seçin</translation>
     </message>
     <message>
         <source> (Default)</source>
@@ -254,22 +352,28 @@
     <name>UserEntry</name>
     <message>
         <source>logged in</source>
-        <translation type="vanished">giriş</translation>
+        <translation type="vanished">Giriş</translation>
     </message>
 </context>
 <context>
     <name>UsersModel</name>
     <message>
+        <location filename="../greeter/usersmodel.cpp" line="52"/>
         <source>Guest Session</source>
         <translation>Misafir Oturumu</translation>
     </message>
     <message>
-        <source>Login</source>
-        <translation>Giriş</translation>
-    </message>
-    <message>
+        <location filename="../greeter/usersmodel.cpp" line="42"/>
+        <location filename="../greeter/usersmodel.cpp" line="44"/>
         <source>Guest</source>
         <translation>Misafir</translation>
+    </message>
+    <message>
+        <location filename="../greeter/usersmodel.cpp" line="70"/>
+        <location filename="../greeter/usersmodel.cpp" line="72"/>
+        <location filename="../greeter/usersmodel.cpp" line="79"/>
+        <source>Login</source>
+        <translation>Giriş</translation>
     </message>
 </context>
 </TS>
