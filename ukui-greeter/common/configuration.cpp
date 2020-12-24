13bcc2d536fe8fdfffc7354afabc8c5fcbe6bd7e
liuyuanpeng <liuyuanpeng@kylinos.cn>
2020-12-24 10:12:42 +0800

    默认背景为/usr/share/backgrounds/warty-final-ubuntukylin.jpg

--- ukui-greeter/common/configuration.cpp
+++ ukui-greeter/common/configuration.cpp
@@ -179,13 +179,15 @@ QPixmap Configuration::getLogo()
 
 QString Configuration::getDefaultBackgroundName()
 {
-
+/*
     if(getSystemDistrib().contains("Ubuntu",Qt::CaseInsensitive))
         return "/usr/share/backgrounds/warty-final-ubuntukylin.jpg";
     else if(getSystemVersion().contains("V10.1",Qt::CaseInsensitive))
         return "/usr/share/backgrounds/warty-final-ubuntukylin.jpg";
     else if(getSystemVersion().contains("V10",Qt::CaseInsensitive))
         return "/usr/share/backgrounds/kylin/kylin-background.png";
-	else 
-		return "/usr/share/backgrounds/warty-final-ubuntukylin.jpg";	
+    else 
+        return "/usr/share/backgrounds/warty-final-ubuntukylin.jpg";	
+*/
+    return "/usr/share/backgrounds/warty-final-ubuntukylin.jpg";
 }
