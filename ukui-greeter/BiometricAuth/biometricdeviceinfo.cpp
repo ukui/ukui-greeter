81ee49605b9414aa0c45e358c90417987753d643
liuyuanpeng <liuyuanpeng@kylinos.cn>
2021-01-14 19:09:53 +0800

    解决生物特征设备读不到的问题

--- ukui-greeter/BiometricAuth/biometricdeviceinfo.cpp
+++ ukui-greeter/BiometricAuth/biometricdeviceinfo.cpp
@@ -113,6 +113,14 @@ QString GetDefaultDevice(const QString &userName)
 //    qDebug() << "configure path: " << settings.fileName();
 
     QString defaultDevice = settings.value("DefaultDevice").toString();
+
+    if(defaultDevice.isEmpty())
+    {
+        QString configPath = QString("/var/lib/lightdm-data/%1/" UKUI_BIOMETRIC_CONFIG_PATH).arg(userName);
+	QSettings settings(configPath, QSettings::IniFormat);
+	defaultDevice = settings.value("DefaultDevice").toString();
+    }
+
     if(defaultDevice.isEmpty())
     {
         QSettings sysSettings(UKUI_BIOMETRIC_SYS_CONFIG_PATH, QSettings::IniFormat);
